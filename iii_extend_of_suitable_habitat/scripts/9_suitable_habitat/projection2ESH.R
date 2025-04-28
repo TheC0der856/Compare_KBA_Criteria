db70235b9f373cc26f534f65756adb8fdc4ccf56
Gronefeld <gronefeld@uni-trier.de>
2025-04-28 17:39:05 +0200

    selected suitable habitat was not calculated in R

--- iii_extend_of_suitable_habitat/scripts/9_suitable_habitat/projection2ESH.R
+++ iii_extend_of_suitable_habitat/scripts/9_suitable_habitat/projection2ESH.R
@@ -3,9 +3,14 @@ library("terra")
 library("sf")
 
 # load projection
-projection <- rast("iii_extend_of_suitable_habitat/Presence/test_projection.tif")
-EMprojection <- projection$Presence_PA1_RUN1_MAXNET
-#plot(EMprojection)
+projection <- rast("iii_extend_of_suitable_habitat/Presence/Cluster/proj_20x20/proj_20x20_Presence_ensemble.tif")
+EMprojectionROC <- projection$Presence_EMcaByROC_mergedData_mergedRun_mergedAlgo
+#EMprojectionTSS <- projection$Presence_EMcaByTSS_mergedData_mergedRun_mergedAlgo
+#plot(EMprojectionROC)
+#plot(EMprojectionTSS)
+# not only the evaluation scores are similar, also the plots
+# since there are no differences I will choose ROC, as it was already choosen for displaying response curves
+EMprojection <- EMprojectionROC
 
 # values between 0 and 1000
 # 0 no way the species occurs here
@@ -20,16 +25,21 @@ suitable_habitat <- mask(EMprojection, EMprojection >= 800 & EMprojection <= 100
 suitable_habitat_poly <- as.polygons(suitable_habitat, dissolve=TRUE)
 suitable_habitat_sf <- st_as_sf(suitable_habitat_poly)
 #plot(suitable_habitat_sf)
-#all(st_is_valid(suitable_habitat_sf)) # controll if errors happend from the transformation
+#all(st_is_valid(suitable_habitat_sf)) # control if errors happened from the transformation
 
 # find the biggest suitable area
 # suitable_habitats_sf <- st_cast(suitable_habitat_sf, "POLYGON")
 # selected_suitable_habitat_sf <- suitable_habitats_sf[which.max(st_area(suitable_habitats_sf)), ]
 
 # find the suitable area within the selected range
-selected_range <- st_read("iv_range/selected_range.shp")
-selected_suitable_habitat_sf <- st_intersection(suitable_habitat_sf, selected_range)
+#selected_range <- st_read("iv_range/selected_range.shp")
+# selected_suitable_habitat_sf <- st_intersection(
+#   suitable_habitat_sf[, "geometry"],
+#   selected_range[, "geometry"]
+# )
+# something is wrong here...
+# I could not find out why it is not working so I made the intersection with jupiter notbook python script.
 
 # save as shapefile
 st_write(suitable_habitat_sf, "iii_extend_of_suitable_habitat/suitable_habitat.shp")
-st_write(selected_suitable_habitat_sf, "iii_extend_of_suitable_habitat/selected_suitable_habitat.shp")
+#st_write(selected_suitable_habitat_sf, "iii_extend_of_suitable_habitat/selected_suitable_habitat.shp")
