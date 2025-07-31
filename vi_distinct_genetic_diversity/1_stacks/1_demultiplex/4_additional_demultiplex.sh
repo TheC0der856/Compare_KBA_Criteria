26b4c34d0fe0f2801b33cdf86e0bbf1b8481113a
Gronefeld <gronefeld@uni-trier.de>
2025-07-31 14:06:41 +0200

    remove SG122

--- vi_distinct_genetic_diversity/1_stacks/1_demultiplex/4_additional_demultiplex.sh
+++ vi_distinct_genetic_diversity/1_stacks/1_demultiplex/4_additional_demultiplex.sh
@@ -6,16 +6,17 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8       # 8 CPUs pro Task
 
-# go to my scatch dir
+# go to my working directory
 cd /scratch/utr_gronefeld/
 
-# starte shell neu
+# start new shell
 source ~/.bashrc
 # activate stacks
 mamba activate Stacks
 
-# Dein eigentlicher Befehl
+# demultiplex
+# SG122 is ignored, somehow it must have been exchanged in the lab and I could not track down the actual ID
 process_radtags -1 outgroup/outgroup_R1_001.fastq -2 outgroup/outgroup_R2_001.fastq -i fastq -b outgroup/barcodes.txt -o combined_analyses/demultiplexed/ -cqr --renz-2 sbfI --renz-1 mseI -t 65 --disable-rad-check --threads 8
 
-# Umgebung wieder deaktivieren
+# deactivate environment
 mamba deactivate
