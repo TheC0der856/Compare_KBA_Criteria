15cb4b95f7761ebb28052ae5586673875adf87b7
Gronefeld <gronefeld@uni-trier.de>
2025-11-25 17:37:16 +0100

    moved

--- vi_distinct_genetic_diversity/4_distinct_genetic_diversity/code/preparation/load_libraries.R
+++
@@ -1,7 +0,0 @@
-#load libraries
-library(dplyr)
-library(vegan)      # Delta+
-library(poppr)      # bitwise.dist()
-library(foreach)
-library(doParallel)
-library(data.table) # at EDplus_for_KBAcombinations
